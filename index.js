d5bf55a5958badbeea2aa9ec35b3ea8f35cdac45
David Gonzalez <dvdgzlez@gmail.com>
2023-07-02 08:39:00 +0200

    adding dotenv, new approach

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
+    res.send("Express + TypeScript Server :D ⚡️");
 });
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at this url http://localhost:${port}`);
 });
